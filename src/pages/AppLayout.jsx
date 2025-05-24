6ebd2da99434d74f924f15a90f9892909c92bd9a
RaghadDeveloper <raghadmh620@gmail.com>
2025-05-24 22:48:32 +0300

    login-logout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
+import User from "../components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
+      <User />
       <Sidebar />
       <Map />
     </div>
